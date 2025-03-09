3dc408f3116a40005cebeaf12a6cea63cbfe5ec1
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2025-03-09 13:15:39 +0200

    Update wait.middleware.ts

--- server/src/middleware/wait.middleware.ts
+++ server/src/middleware/wait.middleware.ts
@@ -4,9 +4,9 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class WaitMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    // Simulate network latency with a 2-second delay
+    // Simulate network latency with delay
     setTimeout(() => {
       next();
-    }, 0);
+    }, 1000);
   }
 }
