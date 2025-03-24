ea1863f6513008a8e6c33c0dc0d9632a1e2e6e40
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2025-03-24 16:12:40 +0200

    Update main-layout.tsx

--- client/src/components/layout/main-layout.tsx
+++ client/src/components/layout/main-layout.tsx
@@ -1,6 +1,4 @@
 import { ReactNode } from "react";
-import { AppSidebar } from "@/components/layout/app-sidebar";
-import { SidebarProvider } from "@/components/ui/sidebar";
 import MainHeader from "@/components/layout/header/main-header";
 
 interface MainLayoutProps {
@@ -10,15 +8,12 @@ interface MainLayoutProps {
 const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <div className="min-h-screen bg-background">
-      <SidebarProvider>
-        {/* <AppSidebar /> */}
-        <div className="grow">
-          <MainHeader />
-          <div className="container px-8 mx-auto mt-12 mb-12 xl:px-36">
-            <main>{children}</main>
-          </div>
+      <div className="grow">
+        <MainHeader />
+        <div className="container px-8 mx-auto mt-12 mb-12 xl:px-36">
+          <main>{children}</main>
         </div>
-      </SidebarProvider>
+      </div>
     </div>
   );
 };
